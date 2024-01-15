07774ac249cc1dac4442689fe81cd9dea6f0c4c9
Dohwan <windboy098@gmail.com>
2024-01-15 16:33:33 +0900

    feat: 특정 api에 인증이 필요하도록 추가

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { ImagesService } from './images.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('images')
 export class ImagesController {
   constructor(private readonly imagesService: ImagesService) {}
 
   @Get('/get-upload-url')
+  @UseGuards(AuthGuard('jwt'))
   getUploadUrl() {
     return this.imagesService.getUploadUrl();
   }
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -6,19 +6,18 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostEntity } from './entity/post.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  /**
-   * TODO: create, update, delete 는 권한 부여해주기
-   */
   @Get()
   getAllPost(): Promise<PostEntity[]> {
     return this.postsService.getAllPost();
@@ -30,16 +29,19 @@ export class PostsController {
   }
 
   @Post()
+  @UseGuards(AuthGuard('jwt'))
   createPost(@Body() postData: CreatePostDto): Promise<PostEntity> {
     return this.postsService.createPost(postData);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard('jwt'))
   updatePost(@Param('id') postId: number, @Body() updateData: UpdatePostDto) {
     return this.postsService.updatePost(postId, updateData);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard('jwt'))
   deletePost(@Param('id') postId: number) {
     return this.postsService.deletePost(postId);
   }
