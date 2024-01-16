b2aa601091b30d31d028b482ff4457d66e07998b
Dohwan <windboy098@gmail.com>
2024-01-16 21:02:04 +0900

    chore: 테스트용 데코레이터 삭제

--- src/auth/get-user.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { UserEntity } from '../users/entity/user.entity';
-
-export const GetUserDecorator = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext): UserEntity =>
-    ctx.switchToHttp().getRequest().user,
-);
