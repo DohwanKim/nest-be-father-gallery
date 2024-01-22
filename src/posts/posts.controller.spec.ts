893e627552441c53f8cf5881b7588b811e0ec53b
Dohwan <windboy098@gmail.com>
2024-01-22 19:36:47 +0900

    chore: 테스트 이그노어 추가 및 아직 작성하지 않는 테스트들 주석 처리

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,18 +1,114 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
+import { NotFoundException } from '@nestjs/common';
+
+const mockService = {
+  getAllPost: jest.fn(),
+  getOnePost: jest.fn(),
+  createPost: jest.fn(),
+  updatePost: jest.fn(),
+  deletePost: jest.fn(),
+};
+
+type MockService = Partial<Record<keyof PostsService, jest.Mock>>;
 
 describe('PostsController', () => {
   let controller: PostsController;
+  let service: MockService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
+      providers: [
+        {
+          provide: PostsService,
+          useValue: mockService,
+        },
+      ],
     }).compile();
 
     controller = module.get<PostsController>(PostsController);
+    service = module.get(PostsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  describe('getAllPost', () => {
+    it('should return an array', async () => {
+      const expectResult = [
+        {
+          id: 2,
+          createAt: '2024-01-18T08:56:02.721Z',
+          updateAt: '2024-01-18T08:56:02.721Z',
+          version: 2,
+          title: 'init title',
+          artType: 'NONE',
+          canvasSize: '100x100',
+          price: 100,
+          frameType: '도화지',
+          contents: '게시글',
+          tags: [],
+          img: null,
+        },
+      ];
+      jest.spyOn(service, 'getAllPost').mockImplementation(() => expectResult);
+
+      const result = await service.getAllPost();
+      expect(service.getAllPost).toHaveBeenCalled();
+      expect(await controller.getAllPost()).toEqual(result);
+    });
+  });
+
+  describe('getOnePost', () => {
+    it('should return a post', async () => {
+      const id = 1;
+      const expectResult = {
+        id,
+        createAt: '2024-01-18T08:56:02.721Z',
+        updateAt: '2024-01-18T08:56:02.721Z',
+        version: 2,
+        title: 'init title',
+        artType: 'NONE',
+        canvasSize: '100x100',
+        price: 100,
+        frameType: '도화지',
+        contents: '게시글',
+        tags: [],
+        img: null,
+      };
+      jest.spyOn(service, 'getOnePost').mockImplementation(() => expectResult);
+
+      const result = await service.getOnePost(id);
+      service.getOnePost.mockReturnValue(result);
+      expect(service.getOnePost).toHaveBeenCalledWith(id);
+      expect(await controller.getOnePost(id)).toEqual(result);
+    });
+
+    // TODO: 의도적으로 에러 발생시켜서 컨트롤러에서 이를 확인해야함.
+    it('should throw NotFoundException if post is not found', async () => {
+      const nonExistingId = 999;
+      jest
+        .spyOn(service, 'getOnePost')
+        .mockRejectedValueOnce(
+          new NotFoundException(`Post id ${nonExistingId} not found`),
+        );
+
+      // try {
+      //   await controller.getOnePost(nonExistingId);
+      // } catch (e) {
+      //   expect(e).toBeInstanceOf(NotFoundException);
+      // }
+
+      await expect(controller.getOnePost(nonExistingId)).rejects.toThrowError(
+        NotFoundException,
+      );
+    });
+  });
+
+  it.todo('createPost');
+  it.todo('updatePost');
+  it.todo('deletePost');
 });
