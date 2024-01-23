d8a2e16186b76178c207a3844dbc1030a20d13ac
Dohwan <windboy098@gmail.com>
2024-01-23 17:04:44 +0900

    test: 이미지 테스트 작성

--- src/images/images.controller.spec.ts
+++ src/images/images.controller.spec.ts
@@ -1,18 +1,36 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { ImagesController } from './images.controller';
-//
-// describe('ImagesController', () => {
-//   let controller: ImagesController;
-//
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [ImagesController],
-//     }).compile();
-//
-//     controller = module.get<ImagesController>(ImagesController);
-//   });
-//
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
+import { Test, TestingModule } from '@nestjs/testing';
+import { ImagesController } from './images.controller';
+import { ImagesService } from './images.service';
+
+const mockService = {
+  getUploadUrl: jest.fn(),
+};
+
+describe('ImagesController', () => {
+  let controller: ImagesController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ImagesController],
+      providers: [{ provide: ImagesService, useValue: mockService }],
+    }).compile();
+
+    controller = module.get<ImagesController>(ImagesController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('getUploadUrl', () => {
+    it('should return an array', async () => {
+      const expectResult = 'https://test.com';
+
+      jest
+        .spyOn(mockService, 'getUploadUrl')
+        .mockImplementation(() => expectResult);
+
+      expect(await controller.getUploadUrl()).toEqual(expectResult);
+    });
+  });
+});
