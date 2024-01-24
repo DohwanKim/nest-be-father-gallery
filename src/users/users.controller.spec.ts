eddcfb6c5b4304c993ae948fd39e522d89c4ed49
Dohwan Kim <windboy2004@naver.com>
2024-01-24 16:35:23 +0900

    test: 유저 컨트롤러 테스트 작성

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,53 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { UsersController } from './users.controller';
-//
-// describe('UsersController', () => {
-//   let controller: UsersController;
-//
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       controllers: [UsersController],
-//     }).compile();
-//
-//     controller = module.get<UsersController>(UsersController);
-//   });
-//
-//   it('should be defined', () => {
-//     expect(controller).toBeDefined();
-//   });
-// });
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+const mockService = {
+  getMyInfo: jest.fn(),
+};
+
+type MockService = Partial<Record<keyof UsersService, jest.Mock>>;
+
+describe('UsersController', () => {
+  let controller: UsersController;
+  let service: MockService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockService,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+    service = module.get(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('getInfo', () => {
+    it('should return a user info', async () => {
+      const id = 1;
+      const req = {
+        user: {
+          id,
+        },
+      };
+      const result = {
+        id,
+        username: 'testman',
+        role: 'USER',
+      };
+
+      jest.spyOn(service, 'getMyInfo').mockImplementation(() => result);
+      expect(controller.getInfo(req)).toEqual(result);
+      expect(service.getMyInfo).toHaveBeenCalledWith(id);
+    });
+  });
+});
