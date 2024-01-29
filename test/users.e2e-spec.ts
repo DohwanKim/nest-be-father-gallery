0a582ad75aa8354d6271146e3bf682814f27193a
Dohwan Kim <windboy2004@naver.com>
2024-01-29 19:54:13 +0900

    fix: 테스트 도중 비밀번호 정규식 잘못 된 것 픽스

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -1,24 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import { DataSource } from 'typeorm';
 
-describe('AppController (e2e)', () => {
+describe('Users (e2e)', () => {
   let app: INestApplication;
+  let dataSource: DataSource;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    dataSource = moduleFixture.get<DataSource>(DataSource);
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await dataSource.destroy();
+    await app.close();
   });
+
+  it.todo('GET /users/info');
 });
