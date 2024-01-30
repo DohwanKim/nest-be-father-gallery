9a46518454c135b8714549eb0c5804363cb7d69b
Dohwan Kim <windboy2004@naver.com>
2024-01-30 17:08:24 +0900

    test: 유저 e2e 테스트 추가

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
 import { DataSource } from 'typeorm';
+import { mockAuthGuard } from './mock-auth-guard';
+import { JwtAccessGuard } from '../src/auth/guards/jwt-access.guard';
+import * as request from 'supertest';
+
+const testUser = {
+  username: 'testuser',
+  password: 'Testpass123!',
+};
 
 describe('Users (e2e)', () => {
   let app: INestApplication;
@@ -10,11 +18,18 @@ describe('Users (e2e)', () => {
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideGuard(JwtAccessGuard)
+      .useValue(mockAuthGuard)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     dataSource = moduleFixture.get<DataSource>(DataSource);
     await app.init();
+    await request(app.getHttpServer()).post('/auth/signup').send({
+      username: testUser.username,
+      password: testUser.password,
+    });
   });
 
   afterAll(async () => {
@@ -22,5 +37,15 @@ describe('Users (e2e)', () => {
     await app.close();
   });
 
-  it.todo('GET /users/info');
+  describe('Get /users/info', () => {
+    it('should get user info', async () => {
+      try {
+        return await request(app.getHttpServer())
+          .get('/users/info')
+          .expect(200);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
 });
