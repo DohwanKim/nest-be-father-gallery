80ba25bac5d152a168d88ad05e78e2b7b39cf5be
Dohwan Kim <windboy2004@naver.com>
2024-01-30 17:07:50 +0900

    chore: 모킹 가드에 데이터를 실제와 같게 수정

--- test/mock-auth-guard.ts
+++ test/mock-auth-guard.ts
@@ -4,9 +4,7 @@ export const mockAuthGuard = {
   canActivate: (context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
 
-    request.user = {
-      id: 1,
-    };
+    request.user = { id: 1, username: 'testuser', role: 'ADMIN' };
 
     return true;
   },
