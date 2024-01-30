29b44598da3d20f01a2ebe3c07f590c994a189f8
Dohwan Kim <windboy2004@naver.com>
2024-01-30 18:01:15 +0900

    test: 유저 e2e 테스트 실패 케이스 추가

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -12,33 +12,29 @@ const testUser = {
 };
 
 describe('Users (e2e)', () => {
-  let app: INestApplication;
   let dataSource: DataSource;
-
-  beforeAll(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    })
-      .overrideGuard(JwtAccessGuard)
-      .useValue(mockAuthGuard)
-      .compile();
-
-    app = moduleFixture.createNestApplication();
-    dataSource = moduleFixture.get<DataSource>(DataSource);
-    await app.init();
-    await request(app.getHttpServer()).post('/auth/signup').send({
-      username: testUser.username,
-      password: testUser.password,
-    });
-  });
 
   afterAll(async () => {
     await dataSource.destroy();
-    await app.close();
   });
 
   describe('Get /users/info', () => {
     it('should get user info', async () => {
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      })
+        .overrideGuard(JwtAccessGuard)
+        .useValue(mockAuthGuard)
+        .compile();
+      dataSource = moduleFixture.get<DataSource>(DataSource);
+
+      const app: INestApplication = moduleFixture.createNestApplication();
+      await app.init();
+      await request(app.getHttpServer()).post('/auth/signup').send({
+        username: testUser.username,
+        password: testUser.password,
+      });
+
       try {
         return await request(app.getHttpServer())
           .get('/users/info')
@@ -46,6 +42,28 @@ describe('Users (e2e)', () => {
       } catch (err) {
         console.log(err);
       }
+
+      await app.close();
+    });
+
+    it('should fail to get user info', async () => {
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
+      dataSource = moduleFixture.get<DataSource>(DataSource);
+
+      const app: INestApplication = moduleFixture.createNestApplication();
+      await app.init();
+
+      try {
+        return await request(app.getHttpServer())
+          .get('/users/info')
+          .expect(401);
+      } catch (err) {
+        console.log(err);
+      }
+
+      await app.close();
     });
   });
 });
